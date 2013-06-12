52d8946506018d54c930372e114d7cfecfd838ca
Danny Coates <dannycoates@gmail.com>
2013-06-11 17:46:10 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	host: '0.0.0.0',
+	port: 8000,
+	elasticsearch: {
+		host: '127.0.0.1',
+		port: 9200
+	}
+}
---
+++ index.js
@@ -0,0 +1,90 @@
+var Hapi = require('hapi')
+var config = require('./config')
+
+var server = new Hapi.Server(config.host, config.port)
+
+server.auth(
+  'simple',
+  {
+    scheme: 'basic',
+    validateFunc: function (name, password, cb) {
+      if (name === 'me' && password === 'TODO') {
+        cb(null, true, { name: name })
+      }
+      else {
+        cb(null, false)
+      }
+    }
+  }
+)
+
+var esHandler = {
+  proxy: {
+    host: config.elasticsearch.host,
+    port: config.elasticsearch.port,
+    protocol: 'http'
+  }
+}
+
+server.route(
+  [
+    {
+      method: 'POST',
+      path: '/{index}/_search',
+      handler: esHandler
+    },
+    {
+      method: 'POST',
+      path: '/{index}/{type}/_search',
+      handler: esHandler
+    },
+    {
+      method: 'GET',
+      path: '/_aliases',
+      handler: esHandler
+    },
+    {
+      method: 'GET',
+      path: '/config.js',
+      handler: function (request) {
+        var x = {
+          elasticsearch: "http://" + request.info.host,
+          kibana_index: "kibana-int",
+          modules: ['histogram','map','pie','table','stringquery','sort',
+                    'timepicker','text','fields','hits','dashcontrol',
+                    'column','derivequeries','trends'],
+        }
+        request
+          .reply('var config = new Settings(' + JSON.stringify(x) + ');')
+          .type('application/javascript')
+      }
+    },
+    {
+      method: '*',
+      path: '/kibana-int/dashboard/{param*}',
+      handler: esHandler,
+      config: {
+        auth: 'simple'
+      }
+    },
+    {
+      method: '*',
+      path: '/kibana-int/{param*}',
+      handler: esHandler,
+      config: {
+        auth: 'simple'
+      }
+    },
+    {
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: './kibana'
+        }
+      }
+    }
+  ]
+)
+
+server.start()
