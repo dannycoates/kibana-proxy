86001a1f3ad47759a7506137d414efab9dc9bc14
Danny Coates <dannycoates@gmail.com>
2013-06-19 15:04:35 -0700

    secure the elasticsearch base url

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ server.route(
       path: '/config.js',
       handler: function (request) {
         var x = {
-          elasticsearch: "http://" + request.info.host,
+          elasticsearch: "https://" + request.info.host,
           kibana_index: "kibana-int",
           modules: ['histogram','map','pie','table','stringquery','sort',
                     'timepicker','text','fields','hits','dashcontrol',
